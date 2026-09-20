c972dc665d82ed331e4875eec87a6da3f9f0ca04
Madhubalaji0112 <madhubalaji0112@gmail.com>
2026-09-20 21:22:40 +0530

    Time: 5 ms (83.36%), Space: 46.2 MB (16.48%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int nrm = x;
+        int rev = 0;
+        while(x>0){
+            rev *= 10;
+            rev += x%10;
+            x /= 10;
+        }
+        if(nrm == rev){ 
+            return true;
+        }
+        return false;
+    }
+}
